0ce315f41860989f66dadb686e89dfc80816edc4
KingJeremyNg <jeremy97ng@gmail.com>
2021-12-08 10:59:00 -0500

    Added pagerank

--- runEval.js
+++ runEval.js
@@ -8,12 +8,9 @@ let cosSim = new CosineSimilarity(data, dictionary, postingsLists);
 cosSim.loadStopWords("./data/common_words");
 // cosSim.setQuery("This is a efficient magnitude poopy");
 cosSim.setQuery("attack on titan");
-// cosSim.setQuery("Garden: Takamine-ke no Nirinka - The Animation");
-cosSim.getRelevantDocs();
-cosSim.calculateIDF();
-cosSim.calculateWeights();
-cosSim.calculateSimilarity();
+cosSim.run();
 
+console.log(cosSim.N);
 console.log(cosSim.query);
 
 var items = Object.keys(cosSim.relDocs).map(function (key) {
@@ -26,4 +23,18 @@ items.sort(function (first, second) {
 
 console.log(items.slice(0, 10));
 
-let cosSim = new CosineSimilarity(data, cosSim.relDocs);
+let pageRank = new PageRank(data, 0.1);
+pageRank.convertIndex();
+pageRank.buildMatrix();
+pageRank.directMatrix();
+pageRank.updateMatrix();
+// console.log(pageRank.reference);
+// console.log(pageRank.N);
+// console.log(pageRank.matrix[pageRank.references["1"]]);
+pageRank.calculateProbability("group0", 2);
+// console.log(pageRank.score);
+for (const [index, score] of items.slice(0, 10)) {
+    console.log(index, pageRank.score[pageRank.references[index]]);
+}
+// console.log(pageRank.matrix[pageRank.references["group0"]]);
+// console.log(pageRank.matrix[pageRank.references["group19000"]]);
--- src/pageRank.js
+++ src/pageRank.js
@@ -1,15 +1,78 @@
+const fs = require('fs');
+
 class PageRank {
-    constructor(data, relDocs) {
+    constructor(data, alpha) {
         this.data = data;
-        this.relDocs = relDocs;
+        this.alpha = alpha;
+        this.references = {};
+        this.N = 0;
+        this.matrix = [];
+        this.score = [];
     }
 
     convertIndex() {
-        
+        this.references = {};
+        let position = 0;
+        for (const [group, animes] of Object.entries(this.data)) {
+            this.references[group] = position;
+            position++;
+            for (const [index, anime] of Object.entries(animes)) {
+                this.references[index] = position;
+                position++;
+            }
+        }
     }
 
     buildMatrix() {
+        this.matrix = [];
+        this.N = Object.keys(this.references).length;
+        for (let i = 0; i < this.N; i++) {
+            this.matrix.push(new Array(this.N).fill(0));
+        }
+    }
 
+    directMatrix() {
+        let prev = null;
+        for (const [group, animes] of Object.entries(this.data)) {
+            if (prev) {
+                this.matrix[this.references[prev]][this.references[group]] = 1;
+                this.matrix[this.references[group]][this.references[prev]] = 1;
+            }
+            prev = group;
+            for (const [index, anime] of Object.entries(animes)) {
+                this.matrix[this.references[group]][this.references[index]] = 1;
+                for (const related of anime["related"]) {
+                    this.matrix[this.references[index]][this.references[related]] = 1;
+                }
+            }
+        }
+    }
+
+    updateMatrix() {
+        for (let i = 0; i < this.N; i++) {
+            for (let j = 0; j < this.N; j++) {
+                this.matrix[i][j] /= this.N;
+                this.matrix[i][j] *= (1 - this.alpha);
+                this.matrix[i][j] += (this.alpha / this.N);
+            }
+        }
+    }
+
+    calculateProbability(start, iterations) {
+        let x = this.matrix[this.references[start]];
+        let result = this.matrix[this.references[start]];
+        for (let i = 1; i < iterations; i++) {
+            result = []
+            for (let j = 0; j < this.N; j++) {
+                let sum = 0;
+                for (let k = 0; k < this.N; k++) {
+                    sum += x[j] * this.matrix[this.references[start]][k];
+                }
+                result.push(sum);
+            }
+            x = result;
+        }
+        this.score = result;
     }
 }
 
